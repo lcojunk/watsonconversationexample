72305e4577d6aeca1ff16bbb860065e09862541a
lcojunk <lcojunk@gmail.com>
2017-10-05 12:02:30 +0200

    changed version number to reflect creation of develop-branch

--- src/main/java/leo/demo/watsonconversation/WatsonconversationApplication.java
+++ src/main/java/leo/demo/watsonconversation/WatsonconversationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WatsonconversationApplication {
-	public static final String DEVELOPMENT_VERSION = "0.0.1";
+	public static final String DEVELOPMENT_VERSION = "0.0.2";
 	public static final long APPLICATION_START_TIME = System.currentTimeMillis();
 
 	public static void main(String[] args) {
